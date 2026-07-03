5bf30b5e9f3baded70217edcfea658af1223d4f5
busik1234 <egortttttttttt@gmail.com>
2026-07-04 04:28:01 +0600

    Задание 4

---
+++ practice2026/task04tests/SpaceshipTests.cs
@@ -0,0 +1,63 @@
+﻿using task04;
+
+namespace task04tests
+{
+    public class SpaceshipTests
+    {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Cruiser();
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(100, cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            ISpaceship fighter = new Fighter();
+            Assert.Equal(100, fighter.Speed);
+            Assert.Equal(40, fighter.FirePower);
+        }
+        [Fact]
+        public void Cruiser_ShouldBeStrongerThanFighter()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.FirePower < cruiser.FirePower);
+        }
+
+        [Fact]
+        public void CruiserFireAndMove()
+        {
+            ISpaceship cruiser = new Cruiser();
+            var exception = Record.Exception(() =>
+            {
+                cruiser.Fire();
+                cruiser.MoveForward();
+            });
+            Assert.Null(exception);
+        }
+        [Fact]
+        public void FighterRotate()
+        {
+            ISpaceship fighter = new Fighter();
+            int angle = 90;
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            fighter.Rotate(angle);
+            string expectedOutput = $"Истребитель повернулся на 90 градусов" + Environment.NewLine;
+            Assert.Equal(expectedOutput, sw.ToString());
+        }
+
+
+    }
+}
