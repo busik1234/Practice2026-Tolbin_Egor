d9d05ad5634d31fe76e320f02f2dd1a052cbea15
busik1234 <egortttttttttt@gmail.com>
2026-07-15 23:38:26 +0600

    Задание 14

---
+++ practice2026/task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using task14;
+using System;
+namespace task14tests
+{
+    using System;
+    using Xunit;
+
+    namespace task14.Tests
+    {
+        public class DefiniteIntegralTests
+        {
+            [Fact]
+            public void Solve_LinearFunctionSymmetricInterval_ReturnsZero()
+            {
+                Func<double, double> X = x => x;
+
+                double result = DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2);
+
+                Assert.Equal(0, result, 1e-4);
+            }
+
+            [Fact]
+            public void Solve_SineFunctionSymmetricInterval_ReturnsZero()
+            {
+                Func<double, double> SIN = x => Math.Sin(x);
+
+                double result = DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8);
+
+                Assert.Equal(0, result, 1e-4);
+            }
+
+            [Fact]
+            public void Solve_LinearFunctionPositiveInterval_ReturnsCorrectValue()
+            {
+                Func<double, double> X = x => x;
+
+                double result = DefiniteIntegral.Solve(0, 5, X, 1e-6, 8);
+
+                Assert.Equal(12.5, result, 1e-5);
+            }
+        }
+    }
+}
